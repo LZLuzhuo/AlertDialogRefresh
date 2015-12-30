aeb0501e2f64ef4b03c033892555097ae8b945c3
LZ-Luzhu <luzhuo@vip.qq.com>
2015-12-30 21:13:15 +0800

    init.

---
+++ src/com/example/dialogrefresh/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.dialogrefresh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * =================================================
+ * 
+ * 作者:卢卓
+ * 
+ * 版本:1.0
+ * 
+ * 创建日期:2015-12-30 下午9:08:16
+ * 
+ * 描述:关于AlertDialog刷新问题的研究
+ * 
+ * 修订历史:
+ * 
+ * 
+ * =================================================
+ **/
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
